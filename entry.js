6f5f9c1fc17be2367bcba7fd762eee68f55716d2
Alvin <alvin@agideo.com>
2016-05-16 00:10:58 +0800

    1st

---
+++ entry.js
@@ -0,0 +1,7 @@
+var Dialog = require("arale-dialog");
+require("!style!css!arale-dialog/src/dialog.css");
+
+var o = new Dialog({
+    trigger: '#trigger',
+    content: '<div>这是 dialog 容器的内容</div>'
+});
